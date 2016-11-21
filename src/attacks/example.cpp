ce95d277900271b55ad3ff0f0ece2de8f7d43146
Mark Turiansky <mark.turiansky@yahoo.com>
2016-11-21 16:07:19 -0500

    example module about done

--- src/attacks/example.cpp
+++ src/attacks/example.cpp
@@ -13,17 +13,25 @@ Example::Example()
     set_opt_value("OUTPUTFILE", "/tmp/cryptotmpfile");
 }
 
+/* helper function that doesn't really do anything */
 void Example::test()
 {
     cout << "example test" << endl;
 }
 
+/* I am overriding the default module function
+ * for displaying the description text
+ */
 void Example::disp_desc()
 {
-    cout << "Module: attacks/example\n\tThis is an example module to show the abilities of the system.\n\tThis is some basic description text that is displayed when show description is input.\n\tPlease define INPUTFILE for this example module to run correctly.\n\tOptions are given below:" << endl;
+    cout << "Module: attacks/example\n\tThis is an example module to show the abilities of the system.\n\tThis is some basic description text that is displayed when show description is input.\n\tThis program will just copy the info from one file to another.\n\tPlease define INPUTFILE for this example module to run correctly.\n\tOptions are given below:" << endl;
     disp_opts();
+    cout << endl;
 }
 
+/* overrides the virtual function from Module
+ * this is where the real meaty stuff happens
+ */
 int Example::run()
 {
     // perform error checking on options first
@@ -36,5 +44,26 @@ int Example::run()
         cout << "[-] Please specify an output file" << endl;
         return 2;
     }
+
+    ifstream in;
+    ofstream out;
+    string buff;
+
+    cout << "[*] Opening file: " << options["INPUTFILE"] << endl;
+    in.open(options["INPUTFILE"]);
+
+    cout << "[*] Opening file: " << options["OUTPUTFILE"] << endl;
+    out.open(options["OUTPUTFILE"]);
+
+    cout << "[*] Writing..." << endl;
+    while (!in.eof()) {
+        getline(in, buff);
+        out << buff;
+    }
+
+    cout << "[*] Closing files" << endl;
+    in.close();
+    out.close();
+
     return 0;
 }
