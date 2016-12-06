ee3222db7f200c979869ffe7135fc36dc9877c0d
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-12-06 02:13:54 -0500

    Rail decryption

--- src/ciphers/rail.cpp
+++ src/ciphers/rail.cpp
@@ -7,14 +7,14 @@ Rail::Rail()
     vector<string> temp;
     temp.push_back("INPUTFILE");
     temp.push_back("OUTPUTFILE");
-    temp.push_back("LENGTH");
+    temp.push_back("NUMRAILS");
     temp.push_back("DECRYPT");
     set_opts(temp);
 
     // set default values, option must exist or error will printed
     set_opt_value("OUTPUTFILE", "encr_rail.txt");
     set_opt_value("DECRYPT", "0");
-	set_opt_value("LENGTH", "5");
+	set_opt_value("NUMRAILS", "5");
 	// delete before merge
 	set_opt_value("INPUTFILE", "makefile");
 }
@@ -24,7 +24,7 @@ Rail::Rail()
  */
 void Rail::disp_desc()
 {
-    cout << "Module: ciphers/rail\n\tEncrypts (or decrypts, if length known) a text file with the rail fence cipher.\n\tPlease define the following required options:\n\t\tINPUTFILE\tinput filename\n\t\tLENGTH\t\tnumber of rails" << endl;
+    cout << "Module: ciphers/rail\n\tEncrypts (or decrypts, if number of rails known) a text file with the rail fence cipher.\n\tPlease define the following required options:\n\t\tINPUTFILE\tinput filename" << endl;
     cout << endl;
 }
 
@@ -36,7 +36,7 @@ int Rail::run()
 	ifstream in;
 	ofstream out;
 	string ibuff, obuff;
-	bool decrypt = false;
+	bool shouldDecrypt = false;
 
 	// perform error checking on options first
 	if (options["INPUTFILE"].empty()) {
@@ -49,13 +49,13 @@ int Rail::run()
 		return 2;
 	}
 
-	if (options["LENGTH"].empty()) {
-		cout << "[-] Please specify a length" << endl;
+	if (options["NUMRAILS"].empty()) {
+		cout << "[-] Please specify the number of rails" << endl;
 		return 3;
 	}
 
 	if (options["DECRYPT"] == "1"){
-		decrypt = true;
+		shouldDecrypt = true;
 	}
 
     cout << "[*] Opening input file: " << options["INPUTFILE"] << endl;
@@ -69,10 +69,14 @@ int Rail::run()
     cout << "[*] Opening output file: " << options["OUTPUTFILE"] << endl;
     out.open(options["OUTPUTFILE"]);
 
-    if (!decrypt) cout << "[*] Encrypting..." << endl;
-    else cout << "[*] Decrypting..." << endl;
-
-	encrypt(&in, &out, stoi(options["LENGTH"]));
+	if (!shouldDecrypt) {
+		cout << "[*] Encrypting..." << endl;
+		encrypt(&in, &out, stoi(options["NUMRAILS"]));
+	}
+	else {
+		cout << "[*] Decrypting..." << endl;
+		decrypt(&in, &out, stoi(options["NUMRAILS"]));
+	}
 
     cout << "[*] Closing files" << endl;
     in.close();
@@ -81,19 +85,18 @@ int Rail::run()
     return 0;
 }
 
-void Rail::encrypt(ifstream* in, ostream* out, int length)
+void Rail::encrypt(ifstream* in, ostream* out, int numRails)
 {
 	vector< vector<char> > rails;
-	rails.resize(length);
+	rails.resize(numRails);
 	char inChar;
 	int currRail = 0;
 	bool ascend = true;
 	while (in->get(inChar)) {
-		cout << inChar;
 		rails[currRail].push_back(inChar);
 		if (ascend) { 
 			currRail++;
-			if (currRail == length-1)
+			if (currRail == numRails-1)
 				ascend = false;
 		}
 		else {
@@ -110,7 +113,7 @@ void Rail::encrypt(ifstream* in, ostream* out, int length)
 			if (currRail > 0) currRail--;
 		}
 	}
-	while (currRail < length) {
+	while (currRail < numRails) {
 		rails[currRail].push_back('\n');
 		currRail++;
 	}
@@ -118,6 +121,73 @@ void Rail::encrypt(ifstream* in, ostream* out, int length)
 	for (vector<char> vec : rails) {
 		for (char output : vec) {
 			(*out) << output;
+		}
+	}
+}
+
+
+void Rail::decrypt(ifstream* in, ostream* out, int numRails)
+{
+	vector< vector<char> > rails;
+	rails.resize(numRails);
+	char inChar;
+
+	string inLine;
+	int length = 0;
+	while (getline(*in, inLine)) {
+		length += inLine.length();
+	}
+	(*in).clear();
+	(*in).seekg(0, ios::beg);
+
+	int numOnEdges = length / ((numRails * 2) - 2);
+	int numInside = numOnEdges * 2 - 1;
+
+	for (int numInLine = 0; numInLine < numOnEdges; numInLine++) {
+		if (in->get(inChar)) {
+			rails[0].push_back(inChar);
+		}
+	}
+	for (int rail = 1; rail < numRails - 1; rail++) {
+		for (int numInLine = 0; numInLine < numInside; numInLine++) {
+			if (in->get(inChar)) {
+				rails[rail].push_back(inChar);
+			}
+		}
+	}
+	for (int numInLine = 0; numInLine < numOnEdges; numInLine++) {
+		if (in->get(inChar)) {
+			rails[numRails-1].push_back(inChar);
+		}
+	}
+
+	int edgeIndex = 0;
+	int middleIndex = 0;
+	int railIndex = 0;
+	bool ascend = true;
+
+	while (edgeIndex < numOnEdges) {
+		if (railIndex == 0 || railIndex == numRails - 1) {
+			(*out) << rails[railIndex][edgeIndex];
+		}
+		else {
+			(*out) << rails[railIndex][middleIndex];
+		}
+
+		if (ascend) {
+			railIndex++;
+			if (railIndex == numRails - 1) {
+				ascend = false;
+				middleIndex++;
+			}
+		}
+		else {
+			railIndex--;
+			if (railIndex == 0) {
+				ascend = true;
+				edgeIndex++;
+				middleIndex++;
+			}
 		}
 	}
 }
